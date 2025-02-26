4e7a2af30c56ddee5ba961f02f805a323097adcf
ArmandoArV <armando.arredondo.valle@gmail.com>
2025-02-25 21:21:17 -0600

    Implements login

--- src/pages/MainPage/main.tsx
+++ src/pages/MainPage/main.tsx
@@ -24,51 +24,59 @@ export const MainPage = () => {
     };
 
     return (
-        <main className={styles.main}>
-            <form className={styles.formContainer} onSubmit={handleSubmit}>
-                <div className={styles.topContainer}>
-                    <div className={styles.logoContainer}>
-                        <div className={styles.imgContainer}>
-                            <img src={logo} alt="logo" className={styles.logo} />
+        <main className={styles.background}>
+            <div className={styles.main}>
+                <form className={styles.formContainer} onSubmit={handleSubmit}>
+                    <div className={styles.topContainer}>
+                        <div className={styles.logoContainer}>
+                            <div className={styles.imgContainer}>
+                                <img src={logo} alt="logo" className={styles.logo} />
+                            </div>
+                        </div>
+                        <div className={styles.headerContainer}>
+                            <h1 className={styles.header}>Iniciar Sesión</h1>
                         </div>
                     </div>
-                    <div className={styles.headerContainer}>
-                        <h1 className={styles.header}>Iniciar Sesión</h1>
+                    <div className={styles.mediumContainer}>
+                        <InputComponent
+                            type="text"
+                            value={emailText}
+                            onChange={handleEmailChange}
+                            placeholder="Correo electrónico"
+                            label="Username"
+                            id="username-input"
+                            className={styles.customInputBorder}
+                            labelClassName={styles.customLabel}
+                        />
+                        <InputComponent
+                            type="password"
+                            value={passwordText}
+                            onChange={handlePasswordChange}
+                            placeholder="Contraseña"
+                            label="Contraseña"
+                            id="password-input"
+                            className={styles.customInputBorder}
+                            labelClassName={styles.customLabel}
+                        />
                     </div>
-                </div>
-                <div className={styles.mediumContainer}>
-                    <InputComponent
-                        type="text"
-                        value={emailText}
-                        onChange={handleEmailChange}
-                        placeholder="Correo electrónico"
-                        label="Username"
-                        id="username-input"
-                        className={styles.customInputBorder}
-                        labelClassName={styles.customLabel}
-                    />
-                    <InputComponent
-                        type="password"
-                        value={passwordText}
-                        onChange={handlePasswordChange}
-                        placeholder="Contraseña"
-                        label="Contraseña"
-                        id="password-input"
-                        className={styles.customInputBorder}
-                        labelClassName={styles.customLabel}
-                    />
-                    <p className={styles.forgotPassword}>¿Olvidaste tu contraseña?</p>
-                </div>
-                <div className={styles.bottomContainer}>
-                    <ButtonComponent
-                        text="Iniciar Sesión"
-                        onClick={() => { }}
-                        className={styles.customButton}
-                        type="submit"
-                    />
-                </div>
-            </form>
+                    <div className={styles.bottomContainer}>
+                        <div className={styles.leftBottomContainer}>
+                            <p className={styles.forgotPassword}>¿Olvidaste tu contraseña?</p>
+                        </div>
+                        <div className={styles.rightBottomContainer}>
+                            <ButtonComponent
+                                text="Iniciar Sesión"
+                                onClick={() => { }}
+                                className={styles.customButton}
+                                type="submit"
+                            />
+                        </div>
+
+                    </div>
+                </form>
+            </div>
         </main>
+
 
     );
 };
