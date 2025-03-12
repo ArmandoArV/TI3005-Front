cdfc9a8db04eb2c28c1fac44117a791ca2d8b8a3
ArmandoArV <armando.arredondo.valle@gmail.com>
2025-03-12 11:14:40 -0600

    Changes lateralnavbar size

--- src/Components/HeaderComponent/HeaderComponent.tsx
+++ src/Components/HeaderComponent/HeaderComponent.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "./main.module.css";
 import logo from "../../resources/Logo.png";
+import { Link } from "react-router-dom";
 
 export const HeaderComponent = () => {
     return (
         <header className={styles.header}>
-            <div className={styles.logoContainer}>
-                <img src={logo} alt="logo" className={styles.logo} />
-            </div>
+            <Link to="/homeAdmin" className={styles.link}>
+                <div className={styles.logoContainer}>
+                    <img src={logo} alt="logo" className={styles.logo} />
+                </div>
+            </Link>
         </header>
     );
 };
